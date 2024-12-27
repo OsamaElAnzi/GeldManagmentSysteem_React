c5c9fdc6faf4dfb37a4beadf32df399bb4e84fe8
osama <129272439+OsamaElAnzi@users.noreply.github.com>
2024-12-27 19:47:13 +0100

    updated

--- GeldApp/src/components/HerbruikbaarCode/LijstTransacties.jsx
+++ GeldApp/src/components/HerbruikbaarCode/LijstTransacties.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Alert, Table, Container } from 'react-bootstrap';
 
 function LijstTransacties({ transacties }) {
--- GeldApp/src/components/Pages/Settings.jsx
+++ GeldApp/src/components/Pages/Settings.jsx
@@ -1,27 +1,34 @@
 import React from "react";
 import ModalSpaarDoel from "../HerbruikbaarCode/ModalSpaarDoel";
-import { Card, Button, Container, Row, Col } from "react-bootstrap";
+import { Card, Container, Row, Col } from "react-bootstrap";
 
 function Settings() {
-  const handleSpaardoel = () => {
-    // Voeg je resetlogica hier toe
-    alert("Instellingen zijn gereset!");
-  };
-
   return (
     <Container className="py-4">
       <h3 className="text-center mb-4">Instellingen</h3>
       <Row xs={1} sm={2} md={3} lg={4} className="g-4">
         <Col>
           <Card>
-            <Card.Header className="bg-success text-white text-center">
-              Reset Instellingen
+            <Card.Header className="bg-success text-white text-center display-6">
+              Jouw spaardoel
             </Card.Header>
             <Card.Body>
               <Card.Text className="text-center">
-                Hier kan je je spaardoel toevoegen of bewerken.
+                Hier kan je je spaardoel bewerken of resetten.
               </Card.Text>
               <ModalSpaarDoel />
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Header className="bg-primary text-white text-center display-6">
+              Jouw vermogen
+            </Card.Header>
+            <Card.Body>
+              <Card.Text className="text-center">
+                Hier kan je je vermogen inzien en bewerken.
+              </Card.Text>
             </Card.Body>
           </Card>
         </Col>
