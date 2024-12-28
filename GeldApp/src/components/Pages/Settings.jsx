afb5853f5b3dcdf594023929500409050f0c1448
osama <129272439+OsamaElAnzi@users.noreply.github.com>
2024-12-28 15:41:50 +0100

    reset knop voor vermogen werkt

--- GeldApp/src/components/Pages/Settings.jsx
+++ GeldApp/src/components/Pages/Settings.jsx
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from "react";
 import ModalSpaarDoel from "../HerbruikbaarCode/ModalSpaarDoel";
 import ModalVermogen from "../HerbruikbaarCode/ModalVermogen";
-import { Card, Container, Row, Col } from "react-bootstrap";
+import { Card, Container, Row, Col, Button, Alert } from "react-bootstrap";
 
 function Settings() {
   const [transacties, setTransacties] = useState([]);
-
+  const [errorMessage, setErrorMessage] = useState("");
+    const [showError, setShowError] = useState(false);
   useEffect(() => {
     const savedTransacties = localStorage.getItem("transactions");
     setTransacties(savedTransacties ? JSON.parse(savedTransacties) : []);
   }, []);
-
+  const handleReset = () => {
+    localStorage.removeItem("transactions");
+    setTransacties([]);
+    setShowError(true);
+    setErrorMessage("Transacties zijn gereset.");
+    setTimeout(() => {
+      setShowError(false);
+    }, 3000);
+  }
   return (
     <Container className="py-4">
       <h3 className="text-center mb-4">Instellingen</h3>
@@ -38,6 +47,13 @@ function Settings() {
                 Hier kan je je vermogen inzien en bewerken.
               </Card.Text>
               <ModalVermogen transacties={transacties} />
+              <Button
+              variant="warning"
+              className="w-100 mt-3"
+              onClick={handleReset}>
+                Reset
+              </Button>
+              {showError && <Alert variant="success" className="mt-3">{errorMessage}</Alert>}
             </Card.Body>
           </Card>
         </Col>
