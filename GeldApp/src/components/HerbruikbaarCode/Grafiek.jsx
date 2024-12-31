4d4a28a0991def2a3716b9710ff586513bf74759
osama <129272439+OsamaElAnzi@users.noreply.github.com>
2024-12-31 16:21:58 +0100

    alleen percentage toevoegen

--- GeldApp/src/components/HerbruikbaarCode/Grafiek.jsx
+++ GeldApp/src/components/HerbruikbaarCode/Grafiek.jsx
@@ -1,66 +1,53 @@
 import React, { useEffect, useState } from "react";
-import { AgCharts } from "ag-charts-react";
+import { Gauge } from "@mui/x-charts";
+import { Typography, Box } from "@mui/material";
 
 const Grafiek = () => {
   const [transactions, setTransactions] = useState([]);
+  const [spaardoel, setSpaardoel] = useState(0);
 
   useEffect(() => {
     const savedTransactions = localStorage.getItem("transactions");
-    if (savedTransactions) {
+    const savedSpaardoel = localStorage.getItem("spaardoel");
+    if (savedTransactions && savedSpaardoel) {
       setTransactions(JSON.parse(savedTransactions));
+      setSpaardoel(JSON.parse(savedSpaardoel));
     }
   }, []);
 
-  const getData = () => {
-    return transactions.map(transaction => ({
-      date: new Date(transaction.datum),
-      inkomen: transaction.type === 'INKOMEN' ? transaction.bedrag : 0,
-      uitgaven: transaction.type === 'UITGAVEN' ? transaction.bedrag : 0,
-    }));
-  };
-
-  const [options, setOptions] = useState({
-    data: [],
-    series: [
-      {
-        type: "line",
-        xKey: "date",
-        yKey: "inkomen",
-        yName: "Inkomen",
-      },
-      {
-        type: "line",
-        xKey: "date",
-        yKey: "uitgaven",
-        yName: "Uitgaven",
-      },
-    ],
-    axes: [
-      {
-        position: "bottom",
-        type: "time",
-        title: {
-          text: "Datum",
-        },
-      },
-      {
-        position: "left",
-        type: "number",
-        title: {
-          text: "Bedrag in Euro",
-        },
-      },
-    ],
-  });
-
-  useEffect(() => {
-    setOptions(prevOptions => ({
-      ...prevOptions,
-      data: getData(),
-    }));
-  }, [transactions]);
-
-  return <AgCharts options={options} />;
+  const totalAmount = transactions.reduce((sum, transaction) => sum + transaction.bedrag, 0);
+  const percentageSpaardoel = spaardoel > 0 ? (totalAmount / spaardoel) * 100 : 0;
+  const percentageSpaardoelDisplay = percentageSpaardoel.toFixed(2);
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      p={3}
+    >
+      <Typography variant="h4" gutterBottom>
+        Spaardoel Vooruitgang
+      </Typography>
+      <Gauge
+        width={300}
+        height={200}
+        value={percentageSpaardoelDisplay}
+        startAngle={-90}
+        endAngle={90}
+        sx={{
+          "& .MuiGauge-valueText": {
+            fontSize: "24px",
+            fontWeight: "bold",
+            color: "#1976d2",
+          },
+        }}
+      />
+      <Typography variant="body1" mt={2}>
+        {percentageSpaardoel.toFixed(2)}% van je doel bereikt
+      </Typography>
+    </Box>
+  );
 };
 
 export default Grafiek;
